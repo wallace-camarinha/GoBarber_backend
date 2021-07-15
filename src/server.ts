4c89b6b58fbd120f8e6705441042b92bd53dd562
Wallace Camarinha <wallace.camarinha@gmail.com>
2021-07-14 23:35:45 -0300

    adicionado entities e typoeorm, feita migration

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import routes from './routes/index';
+
+import './database';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3334, () => {
+  console.log('Server running on port 3334');
+});
