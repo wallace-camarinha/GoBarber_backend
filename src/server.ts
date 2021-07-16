c0f983c1bd7e2131818f9ef504cce1a1e389a722
Wallace Camarinha <wallace.camarinha@gmail.com>
2021-07-16 02:22:57 -0300

    criado UserService e users.routes

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(3334, () => {
-  console.log('Server running on port 3334');
+app.listen(3333, () => {
+  console.log('Server running on port 3333');
 });
