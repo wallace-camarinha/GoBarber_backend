2b7a991f96127cd4c746d54e3c7b99f169099970
Wallace Camarinha <wallace.camarinha@gmail.com>
2021-07-16 00:59:15 -0300

    criado migration e model para users

--- src/database/migrations/1626406262366-CreateUsers.ts
+++ src/database/migrations/1626406262366-CreateUsers.ts
@@ -1,12 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointments1626236751053
-  implements MigrationInterface
-{
+export default class CreateUsers1626406262366 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -16,12 +14,18 @@ export default class CreateAppointments1626236751053
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+            isUnique: true,
           },
           {
             name: 'created_at',
@@ -39,6 +43,6 @@ export default class CreateAppointments1626236751053
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
