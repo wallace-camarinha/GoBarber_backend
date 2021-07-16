c39da526be4de6ed47a3701df66902fdf737acdc
Wallace Camarinha <wallace.camarinha@gmail.com>
2021-07-16 01:51:24 -0300

    criada relação entre appointment e user

--- src/database/migrations/1626236751053-CreateAppointments.ts
+++ src/database/migrations/1626236751053-CreateAppointments.ts
@@ -10,7 +10,7 @@ export default class CreateAppointments1626236751053
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
--- src/database/migrations/1626406262366-CreateUsers.ts
+++ src/database/migrations/1626406262366-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1626406262366 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
