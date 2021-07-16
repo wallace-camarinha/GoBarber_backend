e237cb148af0a7bf93f29175b2af49a76cd8b71e
Wallace Camarinha <wallace.camarinha@gmail.com>
2021-07-16 01:06:41 -0300

    adicionado session route e service

---
+++ src/routes/sessions.routes.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import AuthenticateUserService from '../services/AuthenticateUserService';
+
+const sessionsRouter = Router();
+
+sessionsRouter.post('/', async (request, response) => {
+  try {
+    const { email, password } = request.body;
+    const authUser = new AuthenticateUserService();
+
+    const { user } = await authUser.execute({ email, password });
+
+    return response.json({ user });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default sessionsRouter;
