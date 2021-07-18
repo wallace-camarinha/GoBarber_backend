35248ec1a4552ac86fa1d9749edf2e4926068cae
Wallace Camarinha <wallace.camarinha@gmail.com>
2021-07-18 02:42:02 -0300

    adicionado error handler

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -10,24 +10,20 @@ const usersRouter = Router();
 const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
+  const { name, email, password } = request.body;
 
-    const createUser = new CreateUserService();
-    const user = await createUser.execute({ name, email, password });
+  const createUser = new CreateUserService();
+  const user = await createUser.execute({ name, email, password });
 
-    const userWithoutPassword = {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      created_at: user.created_at,
-      updated_at: user.updated_at,
-    };
+  const userWithoutPassword = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    created_at: user.created_at,
+    updated_at: user.updated_at,
+  };
 
-    return response.json(userWithoutPassword);
-  } catch (err) {
-    return response.status(err.statusCode).json({ error: err.message });
-  }
+  return response.json(userWithoutPassword);
 });
 
 usersRouter.patch(
@@ -35,18 +31,14 @@ usersRouter.patch(
   ensureAuth,
   upload.single('avatar'),
   async (request, response) => {
-    try {
-      const updateUserService = new UpdateUserAvatarService();
+    const updateUserService = new UpdateUserAvatarService();
 
-      const user = await updateUserService.execute({
-        user_id: request.user.id,
-        avatarFilename: request.file?.filename,
-      });
+    const user = await updateUserService.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file?.filename,
+    });
 
-      return response.json(user);
-    } catch (err) {
-      return response.status(err.statusCode).json({ error: err.message });
-    }
+    return response.json(user);
   },
 );
 
--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,8 +1,8 @@
 import { getRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
 
-import User from '../models/User';
 import AppError from '../errors/AppError';
+import User from '../models/User';
 
 interface Request {
   name: string;
