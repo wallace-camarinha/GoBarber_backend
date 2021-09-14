ed7d48e961b1a9b4d4c4834a4d7e8dd20f636782
Wallace Camarinha <wallace.camarinha@gmail.com>
2021-09-13 21:34:59 -0300

    added: ListProviderAppointmentsService

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 import ensureAuth from '@modules/users/infra/http/middlewares/ensureAuth';
 import AppointmentsController from '../controllers/AppointmentsController';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const appointmentsRouter = Router();
 const appointmentController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuth);
 
 appointmentsRouter.post('/', appointmentController.create);
+appointmentsRouter.get('/me', providerAppointmentsController.index);
 
 export default appointmentsRouter;
