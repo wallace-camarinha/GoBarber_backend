8b731e954f4069f2f03757f0d581ae925778d5a0
Wallace Camarinha <wallace.camarinha@gmail.com>
2021-09-14 22:48:09 -0300

    added: SES Mail logic [no credentials]

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'standard e-mail with self domain',
+      name: 'Wallace ',
+    },
+  },
+} as IMailConfig;
